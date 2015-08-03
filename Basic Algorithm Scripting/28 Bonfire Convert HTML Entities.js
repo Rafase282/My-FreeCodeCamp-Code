add951da4e0c7d74bdcee88445a0f4d6b5e332db
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-02 23:29:17 -0400

    More changes

---
+++ Basic Algorithm Scripting/28 Bonfire Convert HTML Entities.js
@@ -0,0 +1,23 @@
+/* Bonfire: Convert HTML Entities
+Difficulty: 2/5
+
+Convert the characters "&", "<", ">", '"' (double quote), and "'" (apostrophe), in a string to their
+corresponding HTML entities.
+
+Remember to use RSAP if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+RegExp HTML Entities
+
+Code by Rafael Rodriguez
+rafase282@gmail.com
+http://www.freecodecamp.com/rafase282 
+*/
+
+function convert(str) {
+  // &colon;&rpar;
+  return str;
+}
+
+convert('Dolce & Gabbana');
