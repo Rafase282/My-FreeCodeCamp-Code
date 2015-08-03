7299c2f7b9a23370633fbbd788d61196835d3db6
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-03 13:33:19 -0400

    DOne

--- Basic Algorithm Scripting/28 Bonfire Convert HTML Entities.js
+++ Basic Algorithm Scripting/28 Bonfire Convert HTML Entities.js
@@ -16,9 +16,31 @@ http://www.freecodecamp.com/rafase282
 */
 
 function convert(str) {
-  // &colon;&rpar;
-  vart temp = str.split(' ');
-  return str;
+    // &colon;&rpar;
+    // Split by character to avoid problems.
+    var temp = str.split('');
+    // Since we are only checking for a few HTML elements I used a switch
+    for (var t in temp){
+        switch (temp[t]){
+            case '<' :
+                temp[t] = '&lt;';
+                break;
+            case '&':
+                temp[t] = '&amp;';
+                break;
+            case '>':
+                temp[t] = '&gt;';
+                break;
+            case '"':
+                temp[t] = '&quot;';
+                break;
+            case "'":
+                temp[t] = '&apos;';
+                break;
+            }
+        }
+  temp = temp.join('');
+  return temp;
 }
 
-convert('Dolce & Gabbana');
+convert("Shindler's List");
