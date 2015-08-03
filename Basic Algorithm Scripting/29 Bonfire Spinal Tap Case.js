8d70753ed4e672871921b5ea43f75caff25c5a07
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-03 16:13:01 -0400

    One more

---
+++ Basic Algorithm Scripting/29 Bonfire Spinal Tap Case.js
@@ -0,0 +1,27 @@
+/* Bonfire: Spinal Tap Case
+Difficulty: 2/5
+
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+Remember to use RSAP if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+RegExp String.replace()
+
+Code by Rafael Rodriguez
+rafase282@gmail.com
+http://www.freecodecamp.com/rafase282 
+*/
+
+function spinalCase(str) {
+	// Create a variable for the white space and underscores.
+	var regex = /\s+|_+/g;
+	// Replace low-upper case to low-space-uppercase
+	str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+	// Replace space and underscore with -
+	return str.replace(regex,'-').toLowerCase();
+}
+
+//spinalCase('The_Andy_Griffith_Show');
+spinalCase('thisIsSpinalTap');
