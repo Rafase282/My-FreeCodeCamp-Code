d66bce09aa76e54b0ff3e7cdebd29d605d18487f
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-05 00:22:38 -0400

    new code

---
+++ Basic Algorithm Scripting/33 Bonfire Finders Keepers.js
@@ -0,0 +1,23 @@
+/* Bonfire: Finders Keepers
+Difficulty 2/5
+
+Create a function that looks through an array (first argument) and returns the first element in the
+array that passes a truth test (second argument).
+
+Remember to use RSAP if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Array.some()
+
+Code by Rafael Rodriguez
+rafase282@gmail.com
+http://www.freecodecamp.com/rafase282 
+*/
+
+function find(arr, func) {
+  var num = 0;
+  return num;
+}
+
+find([1, 2, 3, 4], function(num){ return num % 2 === 0; });
