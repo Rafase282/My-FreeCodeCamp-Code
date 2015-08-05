8e624de024695ba785426de24d89195c430b1bb1
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-04 21:46:45 -0400

    Changes

---
+++ Basic Algorithm Scripting/32 Bonfire Smallest Common Multiple.js
@@ -0,0 +1,24 @@
+/* Bonfire: Smallest Common Multiple
+Difficulty: 2/5
+
+Find the smallest number that is evenly divisible by all numbers in the provided range.
+
+The range will be an array of two numbers that will not necessarily be in numerical order.
+
+Remember to use RSAP if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Smallest Common Multiple
+
+Code by Rafael Rodriguez (rafase282) 
+rafase282@gmail.com 
+http://www.freecodecamp.com/rafase282  
+*/
+
+function smallestCommons(arr) {
+  return arr;
+}
+
+
+smallestCommons([1,5]);
