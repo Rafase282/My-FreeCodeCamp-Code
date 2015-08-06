5b3d80c5d05bdee8001cf24e6a9bfbc478373742
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-06 14:43:41 -0400

    Solved binary problem

--- Basic Algorithm Scripting/36 Bonfire Binary Agents.js
+++ Basic Algorithm Scripting/36 Bonfire Binary Agents.js
@@ -18,9 +18,29 @@ http://www.freecodecamp.com/rafase282
 */
 
 function binaryAgent(str) {
-	return str;
+	// Separate the binary code by space.
+    str = str.split(' ');
+    var power;
+    var decValue = 0;
+    var sentence = '';
+    // Check each binary number from the array.
+    for (var s in str) {
+        // Check each bit from binary number
+        for (var t in str[s]){
+            // This only takes into consideration the active ones.
+            if (str[s][t] == 1){
+            	// This is quivalent to 2 ** position
+                power = Math.pow(2,+str[s].length - t - 1);
+                decValue+= power;
+                // Record the decimal value by adding the number to the previous one.
+            }
+        }
+        // After the binary number is converted to decimal, convert it to string and store
+        sentence+=(String.fromCharCode(decValue));
+        // Reset decimal value for next binary number.
+        decValue = 0;
+    }
+  return sentence;
 }
 
-binaryAgent('01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111
-01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001
-00111111');
+binaryAgent('01001001 00100000 01101100 01101111 01110110 01100101 00100000 01000110 01110010 01100101 01100101 01000011 01101111 01100100 01100101 01000011 01100001 01101101 01110000 00100001')
