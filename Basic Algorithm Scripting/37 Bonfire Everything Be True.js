7ae95641713c082eb53597d6a7f74260a71286f3
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-06 18:27:14 -0400

    More code

---
+++ Basic Algorithm Scripting/37 Bonfire Everything Be True.js
@@ -0,0 +1,41 @@
+/* Bonfire: Everything Be True
+DIfficulty: 2/5
+
+Check if the predicate (second argument) returns truthy (defined) for all elements of a collection
+(first argument).
+
+For this, check to see if the property defined in the second argument is present on every element of
+the collection.
+
+Remember, you can access object properties through either dot notation or [] notation.
+
+Remember to use RSAP if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Object.hasOwnProperty()
+Object.getOwnPropertyNames()
+
+Code by Rafael Rodriguez
+rafase282@gmail.com
+http://www.freecodecamp.com/rafase282 
+*/
+
+function every(collection, pre) {
+  // Create a counter to check how many are true.
+  var counter = 0;
+  // Check for each object
+  for (var c in collection) {
+  	// If it has the same property or the same property value then add 1
+    if (collection[c].hasOwnProperty(pre) || collection[c][pre] == pre){ counter++;}
+  }
+  // Outside the loop, check to see if we got true for all of them and return true or false
+  if (counter == collection.length) {
+    return true;
+  } else 
+    return false;
+}
+
+every([{'user': 'Tinky-Winky', 'sex': 'male'}, {'user': 'Dipsy', 'sex': 'male'}, {'user': 'Laa-Laa',
+'sex': 'female'}, {'user': 'Po', 'sex': 'female'}], 'sex');
+
---
+++ Basic Algorithm Scripting/38 Bonfire Arguments Optional.js
@@ -0,0 +1,28 @@
+/* Bonfire: Arguments Optional
+Difficulty: 2/5
+
+Create a function that sums two arguments together. If only one argument is provided, return a
+function that expects one additional argument and will return the sum.
+
+For example, add(2, 3) should return 5, and add(2) should return a function that is waiting for an
+argument so that var sum2And = add(2); return sum2And(3); // 5
+
+If either argument isn't a valid number, return undefined.
+
+Remember to use RSAP if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Global Function Object
+Arguments object
+
+Code by Rafael Rodriguez
+rafase282@gmail.com
+http://www.freecodecamp.com/rafase282 
+*/
+
+function add() {
+  return false;
+}
+
+add(2,3);
