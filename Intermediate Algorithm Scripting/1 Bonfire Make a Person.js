4d4265424ad353c4d72401daba5b3aeb7a5fcfa0
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-06 23:20:31 -0400

    Layout

---
+++ Intermediate Algorithm Scripting/1 Bonfire Make a Person.js
@@ -0,0 +1,30 @@
+/* Bonfire: Make a Person
+Difficulty: 3/5
+
+Fill in the object constructor with the methods specified in the tests.
+
+Those methods are getFirstName(), getLastName(), getFullName(), setFirstName(first),
+setLastName(last), and setFullName(firstAndLast).
+
+All functions that take an argument have an arity of 1, and the argument will be a string.
+
+These methods must be the only available means for interacting with the object.
+
+Remember to use RSAP if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Closures
+Details of the Object Model
+
+Code by Rafael Rodriguez
+rafase282@gmail.com
+http://www.freecodecamp.com/rafase282 
+*/
+
+var Person = function(firstAndLast) {
+    return firstAndLast;
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
