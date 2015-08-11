a05bcf6952375467f4c78d35fe7ef8fc766c892e
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-11 15:44:49 -0400

    Finished Make Person

--- Intermediate Algorithm Scripting/1 Bonfire Make a Person.js
+++ Intermediate Algorithm Scripting/1 Bonfire Make a Person.js
@@ -23,7 +23,29 @@ http://www.freecodecamp.com/rafase282
 */
 
 var Person = function(firstAndLast) {
-    return firstAndLast;
+	// Instead of creating new functions, create them using this
+    
+    this.fullName = firstAndLast;
+    var arr = this.fullName.split(' ');
+    
+    this.getFirstName = function() {
+        return arr[0];
+    };
+    this.getLastName = function() {
+        return arr[1];
+    };
+    this.getFullName = function() {
+        return firstAndLast;
+    };
+    this.setFirstName = function(first) {
+        arr[0] = first;
+    };
+    this.setLastName = function(last) {
+        arr[1] = last;
+    };
+    this.setFullName = function(firstAndLast) {
+        fullname = firstAndLast;
+    };
 };
 
 var bob = new Person('Bob Ross');
