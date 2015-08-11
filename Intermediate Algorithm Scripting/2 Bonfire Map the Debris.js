0f37514c2102c7cb3c04a1779629bd58b3eebe9e
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-11 15:51:48 -0400

    new problem

---
+++ Intermediate Algorithm Scripting/2 Bonfire Map the Debris.js
@@ -0,0 +1,31 @@
+/* Bonfire: Map the Debris
+Difficulty: 3/5
+
+Return a new array that transforms the element's average altitude into their orbital periods.
+
+The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+
+You can read about orbital periods on wikipedia.
+
+The values should be rounded to the nearest whole number. The body being orbited is Earth.
+
+The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418
+
+Remember to use RSAP if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Math.pow()
+
+Code by Rafael Rodriguez
+rafase282@gmail.com
+http://www.freecodecamp.com/rafase282 
+*/
+
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  return arr;
+}
+
+orbitalPeriod([{name : "sputkin", avgAlt : 35873.5553}]);
