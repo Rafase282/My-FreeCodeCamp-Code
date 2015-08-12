c375a3f56af99ea67838398975aacfed83025233
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-12 19:17:56 -0400

    Code is done

--- Intermediate Algorithm Scripting/3 Bonfire Pairwise.js
+++ Intermediate Algorithm Scripting/3 Bonfire Pairwise.js
@@ -23,7 +23,26 @@ http://www.freecodecamp.com/rafase282
 */
 
 function pairwise(arr, arg) {
-  return arg;
+    var index = [];
+    for (var a in arr){
+        var temp = arr[a];
+        for (var i=1; i < arr.length; i++) {
+            var temp2 = arr[i];
+            //console.log(index);
+            if (temp + temp2 === arg && i > a && index.indexOf(+a) === -1 && index.indexOf(+i) === -1){
+                index.push (+a,+i);
+                console.log(index);
+                break;
+            }
+        }
+    }
+    if (index.length >= 1) {
+        var addAll = function( a,b){
+            return a + b;
+        };
+        return index.reduce(addAll);
+    } else 
+        return 0;
 }
 
-pairwise([1,4,2,3,0,5], 7);
+pairwise([1,1,1], 2);
