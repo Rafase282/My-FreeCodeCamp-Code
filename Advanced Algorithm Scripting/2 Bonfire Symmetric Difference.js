74c96f70bacbab7a4d325ca7894295bfd56b6f12
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-16 01:16:59 -0400

    new file

---
+++ Advanced Algorithm Scripting/2 Bonfire Symmetric Difference.js
@@ -0,0 +1,23 @@
+/* Bonfire: Symmetric Difference
+Difficulty: 4/5
+
+Create a function that takes two or more arrays and returns an array of the symmetric difference of the provided arrays.
+
+The mathematical term symmetric difference refers to the elements in two sets that are in either the first or second set, but not in both.
+
+Remember to use RSAP if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Array.reduce()
+
+ode by Rafael Rodriguez
+rafase282@gmail.com
+http://www.freecodecamp.com/rafase282
+*/
+
+function sym(args) {
+  return arguments;
+}
+
+sym([1, 2, 3], [5, 2, 1, 4]);
