6ce6fd2c03e510f93a3b4fc15c925eee859a5880
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-23 13:37:13 -0400

    Edits
    Ironing everything before adding animation via jQuery

--- Basic Front End Development Projects/Portfolio/js/index.js
+++ Basic Front End Development Projects/Portfolio/js/index.js
@@ -1,13 +1,14 @@
-/* Twitter feed code 
+/* Twitter feed code
  *
  */
-! function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0],
-    p = /^http:/.test(d.location) ? 'http' : 'https';
+!(function (d, s, id) {
+  var js = d.getElementsByTagName(s)[0]
+  var fjs = d.getElementsByTagName(s)[0]
+  var p = /^http:/.test(d.location) ? 'http' : 'https'
   if (!d.getElementById(id)) {
-    js = d.createElement(s);
-    js.id = id;
-    js.src = p + "://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
+    js = d.createElement(s)
+    js.id = id
+    js.src = p + '://platform.twitter.com/widgets.js'
+    fjs.parentNode.insertBefore(js, fjs)
   }
-}(document, "script", "twitter-wjs");
+})(document, 'script', 'twitter-wjs')
