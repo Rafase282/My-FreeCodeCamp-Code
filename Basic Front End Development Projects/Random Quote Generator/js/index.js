9c4e5008b2914aefd75cd8b7fa0c960e60760d7e
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-24 02:02:11 -0400

    Data manipulation

--- Basic Front End Development Projects/Random Quote Generator/js/index.js
+++ Basic Front End Development Projects/Random Quote Generator/js/index.js
@@ -1,6 +1,9 @@
 var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
 var getQuote = function(data) {
   $(".quote-text").text(data.quoteText);
+  if (data.quoteAuthor === '') {
+    data.quoteAuthor = 'Unknown';
+  }
   $(".author-text").text('Author: ' + data.quoteAuthor);
 
 };
