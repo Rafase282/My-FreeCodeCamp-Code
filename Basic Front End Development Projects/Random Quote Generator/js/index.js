8fdb4ea673c49bf988ad1a9e9acf65e341307bd5
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-24 01:14:18 -0400

    New project

---
+++ Basic Front End Development Projects/Random Quote Generator/js/index.js
@@ -0,0 +1,13 @@
+var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
+var getQuote = function(data) {
+  $(".quote-text").text(data.quoteText);
+  $(".author-text").text('Author: ' + data.quoteAuthor);
+
+};
+$(document).ready(function() {
+  $.getJSON(url, getQuote, 'jsonp');
+
+});
+$("#quote").click(function() {
+  $.getJSON(url, getQuote, 'jsonp');
+});
