57d6fd910d56da51973c7d177dcd4be00e644a95
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-25 01:38:40 -0400

    Edits

--- Basic Front End Development Projects/Random Quote Generator/js/index.js
+++ Basic Front End Development Projects/Random Quote Generator/js/index.js
@@ -1,17 +1,18 @@
+// Random Quote Generator
 var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
 var getQuote = function(data) {
   $(".quote-text").text(data.quoteText);
+  var quot = 'https://twitter.com/intent/tweet?text=' + data.quoteText + ' Author ' + data.quoteAuthor +' @Rafase282 goo.gl/2h0NHo';
   if (data.quoteAuthor === '') {
     data.quoteAuthor = 'Unknown';
   }
   $(".author-text").text('Author: ' + data.quoteAuthor);
+  $(".twitter-share-button").attr("href", quot);
 };
 $(document).ready(function() {
   $.getJSON(url, getQuote, 'jsonp');
+
 });
 $("#quote").click(function() {
   $.getJSON(url, getQuote, 'jsonp');
-});
-$("#tweet").click(function() {
-  alert('Soon Bro!');
 });
