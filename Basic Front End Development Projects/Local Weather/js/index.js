fc005109bc3d0c05002a46db1ea7eb59fc43c041
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-29 00:11:28 -0400

    Edits
    I still need to do the background changes

--- Basic Front End Development Projects/Local Weather/js/index.js
+++ Basic Front End Development Projects/Local Weather/js/index.js
@@ -1,15 +1,20 @@
+units = 'imperial';
+
 var getLocation = function(data) {
-  lat = data.lat;
-  lon = data.lon;
-  city = data.city;
-  state = data.regionName;
+  var lat = data.lat;
+  var lon = data.lon;
+  var city = data.city;
+  var state = data.regionName;
   url = 'http://api.openweathermap.org/data/2.5/weather?' + 'lat=' + data.lat + '&lon=' + data.lon + '&units=';
+
   getWeather = function(data) {
-    temp = data.main.temp;
-    description = data.weather[0].description;
-    code = data.weather[0].icon;
-    wspeed = data.wind.speed;
-    html = '<img src="http://openweathermap.org/img/w/' + code + '.png" alt="Weather Icon">' + '<p> ' + Math.round(temp) + ' F, ' + description + '<br> Wind Speed: ' + wspeed + ' mph</p><p>' + city + ', ' + state + '</p>';
+    var temp = data.main.temp;
+    var tempUnit = units === 'metric' ? 'C' : 'F';
+    var windUnit = units === 'metric' ? ' knots' : ' km/h';
+    var description = data.weather[0].description;
+    var code = data.weather[0].icon;
+    var wspeed = data.wind.speed;
+    var html = '<img src="http://openweathermap.org/img/w/' + code + '.png" alt="Weather Icon">' + '<p> ' + Math.round(temp) + ' ' + tempUnit + ', ' + description + '<br> Wind Speed: ' + wspeed + windUnit + '</p><p>' + city + ', ' + state + '</p>';
     $("#weather").html(html);
   };
   $.getJSON(url + 'imperial', getWeather, 'jsonp');
@@ -19,13 +24,10 @@ $(document).ready(function() {
   $.getJSON('http://ip-api.com/json', getLocation, 'jsonp');
   $('input[type=radio][name=farenheit-celcius]').change(function() {
     if ($('#f').is(':checked')) {
-      $.getJSON(url + 'imperial', getWeather, 'jsonp');
-      html = '<img src="http://openweathermap.org/img/w/' + code + '.png" alt="Weather Icon">' + '<p> ' + Math.round(temp) + ' F, ' + description + '<br> Wind Speed: ' + wspeed + ' mph</p><p>' + city + ', ' + state + '</p>';
-      $("#weather").html(html);
+      units = 'imperial'
     } else {
-      $.getJSON(url + 'metric', getWeather, 'jsonp');
-      html = '<img src="http://openweathermap.org/img/w/' + code + '.png" alt="Weather Icon">' + '<p> ' + Math.round(temp) + ' F, ' + description + '<br> Wind Speed: ' + wspeed + ' km/h</p><p>' + city + ', ' + state + '</p>';
-      $("#weather").html(html);
+      units = 'metric'
     }
+    $.getJSON(url + units, getWeather, 'jsonp');
   });
 });
