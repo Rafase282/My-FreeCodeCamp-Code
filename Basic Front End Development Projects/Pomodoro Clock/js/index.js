f6fd1b84ad9094df6f4776e69bb20a2824bfd776
Rafael J. Rodriguez <rafase282@gmail.com>
2015-08-31 18:00:38 -0400

    Edits and progress

--- Basic Front End Development Projects/Pomodoro Clock/js/index.js
+++ Basic Front End Development Projects/Pomodoro Clock/js/index.js
@@ -1,13 +1,56 @@
-//jQuery Variables
 $workT = $("#work-time")
 $sessionT = $("#timer")
 $minutes = $("#min")
 $hours = $("#hour")
 $breakT = $("#break-time")
+$seconds = $("#sec")
+$status = $("#status")
 
-// Controls for Break Length
+var addTime = function(section) {
+  section.text(+section.text() + 1)
+  var min = section.text() % 60
+
+  if (section.text() < 60) {
+    $minutes.text(section.text())
+    if (min == 0 && hour > 1) {
+      $hours.text(hour - 1)
+    }
+  }
+
+  if (section.text() >= 60) {
+
+    if (min == 0) {
+      var hour = +$hours.text() + 1
+      $hours.text(hour)
+    }
+    $minutes.text(min)
+  }
+};
+
+var subTime = function(section) {
+    if (section.text() > 1) {
+      section.text(section.text() - 1)
+      var min = (section.text() % 60)
+      var hour = +$hours.text()
+
+      if (section.text() < 60) {
+        $minutes.text(min)
+        if (min != 0 && hour >= 1) {
+          $hours.text(hour - 1)
+        }
+
+      }
+      if (section.text() >= 60) {
+        $minutes.text(min)
+        if (min == 0 && hour > 1) {
+          $hours.text(hour - 1)
+        }
+      }
+    }
+  }
+  // Controls for Break Length
 $("#minus").click(function() {
-  if ($breakT.text() > 0) {
+  if ($breakT.text() > 1) {
     $breakT.text($breakT.text() - 1)
   }
 });
@@ -18,52 +61,20 @@ $("#plus").click(function() {
 
 // Controls for Session Length
 $("#minus2").click(function() {
-  if ($workT.text() > 0) {
-    $workT.text($workT.text() - 1)
-    var min = ($workT.text() % 60)
-    var hour = +$hours.text()
-
-    if ($workT.text() < 60) {
-      $minutes.text(min)
-      if (min != 0 && hour >= 1) {
-        $hours.text(hour - 1)
-      }
-
-    }
-    if ($workT.text() >= 60) {
-      $minutes.text(min)
-      if (min == 0 && hour > 1) {
-        $hours.text(hour - 1)
-      }
-    }
-  }
+  subTime($workT)
 });
 
 $("#plus2").click(function() {
-  $workT.text(+$workT.text() + 1)
-  var min = $workT.text() % 60
-
-  if ($workT.text() < 60) {
-    $minutes.text($workT.text())
-    if (min == 0 && hour > 1) {
-      $hours.text(hour - 1)
-    }
-  }
-
-  if ($workT.text() >= 60) {
-    if (min == 0) {
-      var hour = +$hours.text() + 1
-      $hours.text(hour)
-    }
-    $minutes.text(min)
-  }
-
+  addTime($workT)
 });
 
 // Controls for Timer
 $("#circle").click(function() {
-  // Work in progress
+
   var timerId // current timer if started
+  var hours = +$hours.text()
+  var minutes = +$minutes.text()
+  var seconds = +$seconds.text()
 
   function clockStart() {
     if (timerId) return
@@ -78,19 +89,31 @@ $("#circle").click(function() {
   }
 
   function update() {
-    var date = new Date()
 
-    var hours = date.getHours()
-    if (hours < 10) hours = '0' + hours
-    document.getElementById('hour').innerHTML = hours
+    if (hours > 0 && minutes == 0) hours--
+      document.getElementById('hour').innerHTML = hours
 
-    var minutes = date.getMinutes()
-    if (minutes < 10) minutes = '0' + minutes
-    document.getElementById('min').innerHTML = minutes
+    if (minutes > 0 && seconds == 0) minutes--
+      document.getElementById('min').innerHTML = minutes
 
-    var seconds = date.getSeconds()
-    if (seconds < 10) seconds = '0' + seconds
-    document.getElementById('sec').innerHTML = seconds
+    if (seconds == 0) {
+      seconds = 60
+      seconds--
+    }
+    if (seconds > 0) seconds--
+      document.getElementById('sec').innerHTML = seconds
+
+    if (hours == 0 && minutes == 0 && seconds == 0 && $status.text() === "Work!") {
+      $status.text("Break")
+      $breakT.text($breakT.text() - 1)
+      addTime($breakT)
+    }
+
+    if (hours == 0 && minutes == 0 && seconds == 0 && $status.text() === "Break") {
+      $status.text("Work!")
+      $workT.text($workT.text() - 1)
+      addTime($workT)
+    }
   }
-
+  clockStart()
 });
