43f9d688c9779e778c26e18b292c90830b550849
Rafael J. Rodriguez <rafase282@gmail.com>
2015-09-05 21:36:43 -0400

    Changed code

--- Intermediate Algorithm Scripting/16 Bonfire Drop it.js
+++ Intermediate Algorithm Scripting/16 Bonfire Drop it.js
@@ -1,29 +1,23 @@
 /* Bonfire: Drop it
 Difficulty: 2/5
-
 Drop the elements of an array (first argument), starting from the front, until the predicate (second
 argument) returns true.
-
 Remember to use RSAP if you get stuck. Try to pair program. Write your own code.
-
 Here are some helpful links:
-
 Arguments object
 Array.shift()
-
 Code by Rafael Rodriguez
 rafase282@gmail.com
-http://www.freecodecamp.com/rafase282 
-*/
+http://www.freecodecamp.com/rafase282
 
+This code no longer works after tests update.
 function drop(arr, func) {
   // Using Array.protptype.filter() which makes it pretty easy.
   return arr.filter(func);
 }
-
 drop([1, 2, 3], function(n) {return n < 3; });
-
-/* Code from Max Helmetag (https://github.com/mhelmetag)
+Code from Max Helmetag (https://github.com/mhelmetag)
+*/
 
 function drop(arr, func) {
   // Drop them elements.
@@ -35,8 +29,8 @@ function drop(arr, func) {
       arr.shift();
     }
   }
+
   return arr;
 }
 
 drop([1, 2, 3], function(n) {return n < 3; });
-*/
