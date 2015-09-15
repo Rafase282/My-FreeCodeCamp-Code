30d653a63193600856f00686b8a9f48ca3c28f77
Rafael J. Rodriguez <rafase282@gmail.com>
2015-09-15 18:23:15 -0400

    CHanges

--- Intermediate Algorithm Scripting/05 Bonfire Search and Replace.js
+++ Intermediate Algorithm Scripting/05 Bonfire Search and Replace.js
@@ -1,4 +1,4 @@
-/* Bonfire: Search and Replace 
+/* Bonfire: Search and Replace
 Difficulty: 2/5
 
 Perform a search and replace on the sentence using the arguments provided and return the new
@@ -21,21 +21,25 @@ Array.splice() String.replace() Array.join()
 
 Code by Rafael Rodriguez
 rafase282@gmail.com
-http://www.freecodecamp.com/rafase282 
+http://www.freecodecamp.com/rafase282
 */
 
 function replace(str, before, after) {
   // Find index where before is on string
+
   var index = str.indexOf(before);
+
   // Check to see if the first letter is uppercase or not
-  if (str[index] === str[index].toUpperCase()){
+  if (str[index] === str[index].toUpperCase()) {
     // Change the after word to be capitalized before we use it.
+
     after = after.charAt(0).toUpperCase() + after.slice(1);
   }
+
   // Now replace the original str with the edited one.
-  str = str.replace(before,after);
-  
- return str;
+  str = str.replace(before, after);
+
+  return str;
 }
 
-replace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+replace('A quick brown fox jumped over the lazy dog', 'jumped', 'leaped');
