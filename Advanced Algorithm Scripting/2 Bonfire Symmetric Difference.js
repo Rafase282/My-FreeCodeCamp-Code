a3fa59df072d0f49345300e185770efa2192f14a
Rafael J. Rodriguez <rafase282@gmail.com>
2015-09-17 15:19:42 -0400

    Done

--- Advanced Algorithm Scripting/2 Bonfire Symmetric Difference.js
+++ Advanced Algorithm Scripting/2 Bonfire Symmetric Difference.js
@@ -16,8 +16,31 @@ rafase282@gmail.com
 http://www.freecodecamp.com/rafase282
 */
 
-function sym(args) {
-  return arguments;
+function sym() {
+
+  // Convert the argument object into a proper array
+  var args = Array.prototype.slice.call(arguments);
+
+  // Return the symmetric difference of 2 arrays
+  var getDiff = function(arr1, arr2) {
+
+    // Returns items in arr1 that don't exist in arr2
+    function filterFunction(arr1, arr2) {
+      return arr1.filter(function(item) {
+        return arr2.indexOf(item) === -1;
+      });
+    }
+
+    // Run filter function on each array against the opposite then get unique values
+    return filterFunction(arr1, arr2)
+      .concat(filterFunction(arr2, arr1))
+      .filter(function(item, idx, arr) {
+        return arr.indexOf(item) === idx;
+      });
+  };
+
+  // Reduce all arguments getting the difference of them
+  return args.reduce(getDiff, []);
 }
 
 sym([1, 2, 3], [5, 2, 1, 4]);
