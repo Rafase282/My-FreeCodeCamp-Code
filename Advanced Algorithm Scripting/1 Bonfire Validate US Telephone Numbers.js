db1a203d0756c7e48acfff9331caaf4e990b84aa
Rafael J. Rodriguez <rafase282@gmail.com>
2015-09-16 23:18:08 -0400

    done

--- Advanced Algorithm Scripting/1 Bonfire Validate US Telephone Numbers.js
+++ Advanced Algorithm Scripting/1 Bonfire Validate US Telephone Numbers.js
@@ -27,10 +27,8 @@ http://www.freecodecamp.com/rafase282
 
 function telephoneCheck(str) {
   // Good luck!
-  // oh lord, again?dsd
-  return true;
+  var re = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})$/;
+  return re.test(str);
 }
-
-
 
 telephoneCheck("555-555-5555");
