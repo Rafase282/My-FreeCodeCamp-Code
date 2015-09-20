ef53106482f7a1f0fbabbe5c3a80903b602f9e0e
Rafael J. Rodriguez <rafase282@gmail.com>
2015-09-20 16:17:29 -0400

    Done

--- Advanced Algorithm Scripting/4 Bonfire Inventory Update.js
+++ Advanced Algorithm Scripting/4 Bonfire Inventory Update.js
@@ -16,6 +16,45 @@ http://www.freecodecamp.com/rafase282
 
 function inventory(arr1, arr2) {
   // All inventory must be accounted for or you're fired!
+
+  var index;
+  var arrCurInvName = []; // Names of arr1's items
+  var arrNeInvName = []; // Names of arr2's items
+
+  // Same as using two for loops, this takes care of increasing the number of stock quantity.
+  arr1.map(function(item1) {
+    return arr2.map(function(item2) {
+      if (item1[1] === item2[1]) {
+        item1[0] = item1[0] + item2[0]; //Increase number of stock
+      }
+    });
+  });
+
+  // Get item's name for new Inventory
+  arr2.map(function(item) {
+    arrNeInvName.push(item[1]);
+  });
+
+  // Get item's name for Current Inventory
+  arr1.map(function(item) {
+    arrCurInvName.push(item[1]);
+  });
+
+  // Add new inventory items to current inventory.
+  arrNeInvName.map(function(item) {
+    if (arrCurInvName.indexOf(item) === -1) {
+      index = arrNeInvName.indexOf(item);
+      arr1.push(arr2[index]);
+    }
+  });
+
+  // Sort the array alphabetically using the second element of the array as base.
+  arr1.sort(function(currItem, nextItem) {
+
+    //Ternary function to avoid using if else
+    return currItem[1] > nextItem[1] ? 1 : -1;
+  });
+
   return arr1;
 }
 
