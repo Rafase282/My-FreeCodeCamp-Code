a17b15fd0fa9c0c35f971e6b0d56ae5343df4b34
Rafael J. Rodriguez <rafase282@gmail.com>
2015-09-19 21:25:29 -0400

    soon

---
+++ Advanced Algorithm Scripting/5 Bonfire No repeats please.js
@@ -0,0 +1,24 @@
+/* Bonfire: No repeats please
+Difficulty: 4/5
+
+Return the number of total permutations of the provided string that don't have repeated consecutive letters.
+
+For example, 'aab' should return 2 because it has 6 total permutations, but only 2 of them don't have the same letter (in this case 'a') repeating.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Permutations
+RegExp
+
+Code by Rafael Rodriguez
+rafase282@gmail.com
+http://www.freecodecamp.com/rafase282
+*/
+
+function permAlone(str) {
+  return str;
+}
+
+permAlone('aab');
