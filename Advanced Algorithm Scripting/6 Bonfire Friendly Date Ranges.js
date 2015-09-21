9f577881f3175300311cee24f8b19fea7a8dc692
Rafael J. Rodriguez <rafase282@gmail.com>
2015-09-21 15:47:31 -0400

    Start

---
+++ Advanced Algorithm Scripting/6 Bonfire Friendly Date Ranges.js
@@ -0,0 +1,31 @@
+/*Bonfire: Friendly Date Ranges
+Difficulty: 4/5
+
+Implement a way of converting two dates into a more friendly date range that could be presented to a user.
+
+It must not show any redundant information in the date range.
+
+For example, if the year and month are the same then only the day range should be displayed.
+
+Secondly, if the starting year is the current year, and the ending year can be inferred by the reader, the year should be omitted.
+
+Input date is formatted as YYYY-MM-DD
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+String.split()
+String.substr()
+parseInt()
+
+Code by Rafael Rodriguez
+rafase282@gmail.com
+http://www.freecodecamp.com/rafase282
+*/
+
+function friendly(str) {
+  return str;
+}
+
+friendly(['2015-07-01', '2015-07-04']);
