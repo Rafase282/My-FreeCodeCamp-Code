01b78a1ea78e58c2ffda696feee6ce908adf342d
Rafael J. Rodriguez <rafase282@gmail.com>
2015-09-21 15:21:27 -0400

    Done

--- Advanced Algorithm Scripting/5 Bonfire No repeats please.js
+++ Advanced Algorithm Scripting/5 Bonfire No repeats please.js
@@ -18,7 +18,44 @@ http://www.freecodecamp.com/rafase282
 */
 
 function permAlone(str) {
-  return str;
+
+  // Create a regex to match repeated consecutive characters.
+  var regex = /(.)\1+/g;
+
+  // Split the string into an array of characters.
+  var arr = str.split('');
+  var permutations = [];
+  var tmp;
+
+  // FUnction to swap variables' content.
+  function swap(index1, index2) {
+    tmp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = tmp;
+  }
+
+  //Generate arrays of permutations using the algorithm.
+  function generate(int) {
+    if (int === 1) {
+      // Make sure to join the characters as we create  the permutation arrays
+      permutations.push(arr.join(''));
+    } else {
+      for (var i = 0; i != int; ++i) {
+        generate(int - 1);
+        swap(int % 2 ? 0 : i, int - 1);
+      }
+    }
+  }
+
+  generate(arr.length);
+
+  // Filter the array of repeated permutations.
+  var filtered = permutations.filter(function(string) {
+    return !string.match(regex);
+  });
+
+  //Return how many have no repetitions.
+  return filtered.length;
 }
 
 permAlone('aab');
