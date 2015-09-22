bf8d5bae6bc7b64a2a6e7aacd19f700c8b084c64
Rafael J. Rodriguez <rafase282@gmail.com>
2015-09-22 16:36:54 -0400

    Work in progress

--- Advanced Algorithm Scripting/6 Bonfire Friendly Date Ranges.js
+++ Advanced Algorithm Scripting/6 Bonfire Friendly Date Ranges.js
@@ -25,6 +25,47 @@ http://www.freecodecamp.com/rafase282
 */
 
 function friendly(str) {
+  var date1 = str[0].split('-');
+  var date2 = str[1].split('-');
+  var year = [];
+  var month = [];
+  var day = [];
+
+  function checkYear() {
+    if (date1[0] != date2[0]) {
+      year.push(date1[0]);
+      year.push(date2[0]);
+    } else {
+      year.push(date1[0]);
+    }
+
+  }
+
+  function checkMonth() {
+    if (date1[1] != date2[1]) {
+      month.push(date1[1]);
+      month.push(date2[1]);
+    } else {
+      month.push(date1[1]);
+    }
+
+  }
+
+  function checkDay() {
+    if (date1[2] != date2[2]) {
+      day.push(date1[2]);
+      day.push(date2[2]);
+    } else {
+      day.push(date1[2]);
+    }
+
+  }
+
+  checkYear();
+  checkMonth();
+  checkDay();
+
+  console.log(year, month, day);
   return str;
 }
 
