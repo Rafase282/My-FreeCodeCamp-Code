7c11185314553543492940a1eadc520e8e96000e
Rafael J. Rodriguez <rafase282@gmail.com>
2015-09-27 15:29:38 -0400

    Add comments

--- Intermediate Algorithm Scripting/01 Bonfire Sum All Numbers in a Range.js
+++ Intermediate Algorithm Scripting/01 Bonfire Sum All Numbers in a Range.js
@@ -20,9 +20,12 @@ http://www.freecodecamp.com/rafase282
 */
 
 function sumAll(arr) {
+  // Find and set the maximum and minimun between the array.
   var max = Math.max(arr[0], arr[1]);
   var min = Math.min(arr[0], arr[1]);
   var temp = 0;
+
+  // Create a for loop that will go form the minimun to the maimun and add each number as it iterates.
   for (var i = min; i <= max; i++) {
     temp += i;
   }
