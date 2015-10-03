b4cf8bd6dd1438d83bca03d8689ddbc2d1250558
Rafael J. Rodriguez <rafase282@gmail.com>
2015-10-03 19:54:56 -0400

    Edits

--- Basic Front End Development Projects/Local Weather/js/index.js
+++ Basic Front End Development Projects/Local Weather/js/index.js
@@ -1,62 +1,74 @@
 /*global $, url, getWeather*/
-var units = 'imperial'
+var units = 'imperial';
 
 // Function to work with Location API to get Longitude, Latitude, City and State to bed used with the weather API
-var getLocation = function (data) {
-  var lat = data.lat
-  var lon = data.lon
-  var city = data.city
-  var state = data.regionName
+var getLocation = function(data) {
+  var lat = data.lat;
+  var lon = data.lon;
+  var city = data.city;
+  var state = data.regionName;
+
   // Custom url for the weather API, it is only missing imperial or metric format.
-  url = 'http://api.openweathermap.org/data/2.5/weather?' + 'lat=' + lat + '&lon=' + lon + '&units='
+
+  url = 'http://api.openweathermap.org/data/2.5/weather?' + 'lat=' + lat + '&lon=' + lon + '&units=';
 
   // Function to get the Weather info and display it.
-  getWeather = function (data) {
-    var temp = data.main.temp
-    var tempUnit = units === 'metric' ? 'C' : 'F'
-    var windUnit = units === 'metric' ? ' meters/s' : ' miles/h'
-    var description = data.weather[0].description
-    var code = data.weather[0].icon
-    var wspeed = data.wind.speed
+  getWeather = function(data) {
+    var temp = data.main.temp;
+    var tempUnit = units === 'metric' ? 'C' : 'F';
+    var windUnit = units === 'metric' ? ' meters/s' : ' miles/h';
+    var description = data.weather[0].description;
+    var code = data.weather[0].icon;
+    var wspeed = data.wind.speed;
+
     // Create custom HTML to display all the information gathered.
-    var html = '<img src="http://openweathermap.org/img/w/' + code + '.png" alt="Weather Icon">' + '<p> ' + Math.round(temp) + ' ' + tempUnit + ', ' + description + '<br> Wind Speed: ' + wspeed + windUnit + '</p><p>' + city + ', ' + state + '</p>'
+    var html = '<img src="http://openweathermap.org/img/w/' + code + '.png" alt="Weather Icon">' + '<p> ' + Math.round(temp) + ' ' + tempUnit + ', ' + description + '<br> Wind Speed: ' + wspeed + windUnit + '</p><p>' + city + ', ' + state + '</p>';
+
     // Displays the custom HTML
-    $('#weather').html(html)
+
+    $('#weather').html(html);
+
     // Checks what kind style of temperature was used for dynamic background image.
     switch (tempUnit) {
       case 'F':
-        var temps = [90, 70, 32]
-        break
+        var temps = [90, 70, 32];
+        break;
       case 'C':
-        temps = [32, 21, 0]
-        break
+        temps = [32, 21, 0];
+        break;
     }
+
     // Array of backgroudn images.
-    var imgs = ['url("http://i.imgur.com/eI5KLUW.jpg")', 'url("http://i.imgur.com/rG0P1ro.jpg")', 'url("http://i.imgur.com/voCuONs.jpg")', 'url("http://i.imgur.com/5tFHSKa.jpg")']
+    var imgs = ['url("http://i.imgur.com/eI5KLUW.jpg")', 'url("http://i.imgur.com/rG0P1ro.jpg")', 'url("http://i.imgur.com/voCuONs.jpg")', 'url("http://i.imgur.com/5tFHSKa.jpg")'];
+
     // Select custom backgroudn image according to temperature range.
     if (temp >= temps[0]) {
-      $('body').css('background-image', imgs[0])
+      $('body').css('background-image', imgs[0]);
     } else if (temp < temps[0] && temp >= temps[1]) {
-      $('body').css('background-image', imgs[1])
+      $('body').css('background-image', imgs[1]);
     } else if (temp < temps[1] && temp >= temps[2]) {
-      $('body').css('background-image', imgs[2])
+      $('body').css('background-image', imgs[2]);
     } else if (temp < temps[2]) {
-      $('body').css('background-image', imgs[3])
+      $('body').css('background-image', imgs[3]);
     }
-  }
+  };
+
   // Calls the Weather API
-  $.getJSON(url + 'imperial', getWeather, 'jsonp')
-}
+  $.getJSON(url + 'imperial', getWeather, 'jsonp');
+};
+
 // When the documet finished loading call the Location API
-$(document).ready(function () {
-  $.getJSON('http://ip-api.com/json', getLocation, 'jsonp')
+$(document).ready(function() {
+  $.getJSON('http://ip-api.com/json', getLocation, 'jsonp');
+
   // Handler for opetion between Metric and Imperial style temperature
-  $('input[type=radio][name=farenheit-celcius]').change(function () {
+  $('input[type=radio][name=farenheit-celcius]').change(function() {
     if ($('#f').is(':checked')) {
-      units = 'imperial'
+      units = 'imperial';
     } else {
-      units = 'metric'
-    }
-    $.getJSON(url + units, getWeather, 'jsonp')
-  })
-})
+      units = 'metric';
+    };
+
+    $.getJSON(url + units, getWeather, 'jsonp');
+  });
+});
