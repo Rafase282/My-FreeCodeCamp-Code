65f64c568290659f6c75a0649ef5e6a8ad169804
Rafael J. Rodriguez <rafase282@gmail.com>
2015-10-03 20:01:11 -0400

    edit

--- Intermediate Front End Development Projects/Camper News/js/index.js
+++ Intermediate Front End Development Projects/Camper News/js/index.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
   var url = 'http://www.freecodecamp.com/news/hot';
+  $.getJSON(url, getNews, 'jsonp');
 
 });
+
+function getNews(data) {
+  var id = data.id;
+
+};
