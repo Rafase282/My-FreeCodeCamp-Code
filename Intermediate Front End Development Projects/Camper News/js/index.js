0eb726b0a5b2e0e2311e7364f07fcc7f89903da4
Rafael J. Rodriguez <rafase282@gmail.com>
2015-10-03 21:32:37 -0400

    edits

--- Intermediate Front End Development Projects/Camper News/js/index.js
+++ Intermediate Front End Development Projects/Camper News/js/index.js
@@ -5,17 +5,21 @@ $(document).ready(function() {
   var headline = 'Undefined';
   var link = '';
   var upvotes = 0;
+  var fcc = 'http://freecodecamp/';
+  var time = 0;
+  var html;
 
   $.getJSON(url, function(data) {
-    var items = [];
-    console.log(data);
     for (var news in data) {
-      username = news.author.username;
-      picture = news.author.picture;
-      headline = news.headline;
-      link = news.link;
-      upvotes = news.upVotes.length;
+      username = data[news].author.username;
+      picture = data[news].author.picture;
+      headline = data[news].headline;
+      link = data[news].link;
+      upvotes = data[news].upVotes.length;
+      time = new Date(data[news].timePosted);
+      html = '<div class= "user"><img class= \'logo\' src = "' + picture + '">' + ' <a href="' + fcc + username + '" target="_blank"><p class= \'stat\'>' + 'by ' + username + ' (<i class="fa fa-fire fa-fw"></i>)</p></a>' + ' <a href="' + link + '" target="_blank"><p class= \'stat\'>' + headline + '</p></a>' + '<p class= \'stat\'> <span class="glyphicon glyphicon glyphicon-arrow-up"></span> ' + upvotes + ' Posted on: ' + time + '</p></div>';
 
+      $('.list').append(html);
     }
   });
 });
