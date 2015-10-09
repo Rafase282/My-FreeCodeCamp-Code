5c9e6916f13aa845083040d1660db4a5033eedbd
Rafael J. Rodriguez <rafase282@gmail.com>
2015-10-09 14:48:02 -0400

    Saving progress

--- Intermediate Front End Development Projects/Wikipedia Viwer/js/index.js
+++ Intermediate Front End Development Projects/Wikipedia Viwer/js/index.js
@@ -1,1 +1,51 @@
-$(document).ready(function() { $('.text-primary').text('Hi CodePen!') });
+var url = 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=rafael';
+var query = $('#search').val();
+
+function search() {
+
+  // Option 1
+  $.getJSON(url + query, function(response) {
+    console.log(data);
+    var title = data.query.pages[0].title;
+    console.log(title);
+  });
+
+  // Option 2
+  $.ajax({
+    url: url,
+    action: 'query',
+    list: 'search',
+    format: 'json',
+    srsearch: query,
+    dataType: 'jsonp',
+    type: 'POST',
+    headers: {
+      'Api-User-Agent': 'Example/1.0'
+    },
+    success: function(data) {
+      // do something with data
+      console.log(data);
+      var title = data.query.pages[0].title;
+      console.log(title);
+    }
+  });
+
+  if ($('#search').val().length > 0) {
+    // Display matching names by hiding anything that is not what we want from the  class= "user"
+    var reg = new RegExp($('#search').val(), 'ig');
+    $('.user').css('display', 'none');
+
+  } else if ($('#search').val().length < 1) {
+    // display everything again
+    $('.user').css('display', 'block');
+  }
+
+  $('#search').unbind('keyup');
+  $('#search').keyup(function() {
+    search();
+  });
+}
+
+$('#search').keyup(function() {
+  search();
+});
