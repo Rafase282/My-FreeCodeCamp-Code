b57fee502243d05c164b4a040557224d650dceb8
Rafael J. Rodriguez <rafase282@gmail.com>
2015-10-27 20:13:34 -0400

    Working frontend

--- Intermediate Front End Development Projects/TicTacToe/js/index.js
+++ Intermediate Front End Development Projects/TicTacToe/js/index.js
@@ -1,1 +1,12 @@
-$(document).ready(function() { $('.text-primary').text('Hi CodePen!') });
+var player = 'X';
+var machine = 'O';
+$('.icon').on("click", function() {
+  if ($(this).attr('id') === 'x') {
+    player = 'X';
+    machine = 'O';
+  } else {
+    player = 'O';
+    machine = 'X';
+  }
+  console.log(player);
+});
