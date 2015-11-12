e39e63760a387fab178375a2342014fe84e4f2db
Rafael J. Rodriguez <rafase282@gmail.com>
2015-11-11 19:35:58 -0500

    Game Progress

---
+++ Intermediate Front End Development Projects/Simon/README.txt
@@ -0,0 +1,15 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/Rafase282/pen/JYeVmx.
+
+ Web App with the following features:
+
+1. As a user, I am presented with a random series of button presses.
+2. As a user, each time I input a series of button presses correctly, I see the same series of button presses but with an additional step.
+3. As a user, I hear a sound that corresponds to each button both when the series of button presses plays, and when I personally press a button.
+4. As a user, if I press the wrong button, I am notified that I have done so, and that series of button presses starts again to remind me of the pattern so I can try again.
+5. As a user, I can see how many steps are in the current series of button presses.
+6. As a user, if I want to restart, I can hit a button to do so, and the game will return to a single step.
+7. As a user, I can play in strict mode where if I get a button press wrong, it notifies me that I have done so, and the game restarts at a new random series of button presses.
+8. As a user, the tempo of the game speeds up incrementally on the 5th, 9th and 13th step.
+9. As a user, I can win the game by getting a series of 20 steps correct. I am notified of my victory, then the game starts over.
+
+Forked from [Rafael J. Rodriguez](http://codepen.io/Rafase282/)'s Pen [Simon Says!](http://codepen.io/Rafase282/pen/meQQGQ/).
--- Intermediate Front End Development Projects/Simon/js/index.js
+++ Intermediate Front End Development Projects/Simon/js/index.js
@@ -2,29 +2,30 @@
 var sequence = [1, 2, 3, 4];
 var player = [];
 var strict = false;
-var round = 0;
+var count = 0;
 var speed = 600;
+var audio1 = new Audio(
+  'http://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+var audio2 = new Audio(
+  'http://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+var audio3 = new Audio(
+  'http://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+var audio4 = new Audio(
+  'http://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
 
-// Power Button
 $('.power-button').on('click', function() {
   $(this).toggleClass('float-left float-right');
-  //$(this).hasClass('float-right') ? on() : off();
   NewRound(sequence, speed);
 });
 
-// Strict Button
 $('.strict-button').on('click', function() {
   $(this).toggleClass('brighten');
-  $(this).hasClass('brighten') ? strict = true : strict = false;
-  console.log(strict);
 });
 
-// Start Button
 $('.start-button').on('click', function() {
   $(this).toggleClass('brighten');
 });
 
-// Handling tiles
 $('.tiles').on('click', function() {
   $(this).toggleClass('brighten');
 });
@@ -33,7 +34,6 @@ function NewRound(sequence, speed) {
   // Adds new random color and sends the sequence to be animated
   var color = getColor(4);
   sequence.push(color);
-  console.log(sequence);
 
   //Must increase speed on 5th, 9th and 13th round
   animate(sequence, speed);
@@ -52,7 +52,9 @@ function animate(sequence, speed) {
 }
 
 function LightUp(tile) {
-  // body...
+  // Light the button for a short while
+  var audio = 'audio' + tile;
+  beep(audio);
   var $tile = $('[data-tile=' + tile + ']').addClass('brighten');
   window.setTimeout(function() {
     $tile.removeClass('brighten');
@@ -64,4 +66,22 @@ function getColor(num) {
   Green: 1   Red: 2   Blue: 3   Yellow: 4
   */
   return Math.floor(Math.random() * num) + 1;
+}
+
+function beep(audio) {
+  // body...
+  switch (audio) {
+    case 'audio1':
+      audio1.play();
+      break;
+    case 'audio2':
+      audio2.play();
+      break;
+    case 'audio3':
+      audio3.play();
+      break;
+    case 'audio4':
+      audio4.play();
+      break;
+  }
 }
