1064caab866b44f6bf4c2b046f58eb5a70b6534f
Rafael J. Rodriguez <rafase282@gmail.com>
2016-02-18 15:07:28 -0500

    Added another solution:

--- Intermediate Algorithm Scripting/05 Bonfire Search and Replace.js
+++ Intermediate Algorithm Scripting/05 Bonfire Search and Replace.js
@@ -43,3 +43,16 @@ function replace(str, before, after) {
 }
 
 replace('A quick brown fox jumped over the lazy dog', 'jumped', 'leaped');
+
+
+// Another solution I came up with giving it another try
+
+function myReplace(str, before, after) {
+  if (before[0] === before[0].toUpperCase()) {
+    after = after[0].toUpperCase() + after.substring(1, after.length);
+  }
+
+  return str.replace(before, after);
+}
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
