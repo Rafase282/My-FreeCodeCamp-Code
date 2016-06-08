d12eed55272d0b27afd572e669770176f175820b
Rafael J. Rodriguez <rafase282@gmail.com>
2016-06-08 13:01:15 -0400

    Adds basic solution

--- Basic Algorithm Scripting/10 Bonfire Truncate a string.js
+++ Basic Algorithm Scripting/10 Bonfire Truncate a string.js
@@ -18,6 +18,24 @@ rafase282@gmail.com
 http://www.freecodecamp.com/rafase282
 */
 
+// Basic solution
+
+
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if (str.length > num && num > 3) {
+    return str.slice(0, (num - 3)) + '...';
+  } else if (str.length > num && num <= 3) {
+    return str.slice(0, num) + '...';
+  } else {
+    return str;
+  }
+
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
+
+// Advanced solution
 
 function truncateString(str, num) {
   // Clear out that junk in your trunk
