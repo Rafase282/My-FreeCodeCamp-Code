383b4030ae68293d10afcb283ee679b715c5867e
Rafael J. Rodriguez <rafase282@gmail.com>
2016-06-15 12:58:25 -0400

    Update caesars cipher

--- Basic Algorithm Scripting/17 Bonfire Caesars Cipher.js
+++ Basic Algorithm Scripting/17 Bonfire Caesars Cipher.js
@@ -27,14 +27,13 @@ function rot13(str) { // LBH QVQ VG!
   function decode(arg) {
     // Takes care of characters that are not in [A-Z] such as ! and ? and decodes [A-Z]
     var decoded = 0;
-    if (arg < 65 || arg > 90) {
-      return String.fromCharCode(arg);
-    } else {
+    if (arg >= 65 && arg <= 90) {
       decoded = (arg + 13) % 91;
+      return String.fromCharCode(decoded < 65 ? decoded += 65 : decoded);
     }
-    return String.fromCharCode(decoded < 65 ? decoded += 65 : decoded);
+    return String.fromCharCode(arg);
   }
-
 }
+
 // Change the inputs below to test
-rot13("SERR CVMMN!");
+rot13("SERR PBQR PNZC");
