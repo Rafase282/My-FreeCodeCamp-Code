9e6358ec100558e4696b16fd04ca4876fe9f30eb
Rafael J. Rodriguez <rafase282@gmail.com>
2016-08-15 16:02:22 -0400

    Updates code and description

--- Checkpoints/Checkpoint Record Collection.js
+++ Checkpoints/Checkpoint Record Collection.js
@@ -1,21 +1,27 @@
 /* Checkpoint: Record Collection
 
-You are given a JSON object representing (a small part of) your record collection. Each album is identified by a unique id number and has several properties. Not all albums have complete information.
+You are given a JSON object representing a part of your musical album collection. Each album has several properties and a unique id number as its key. Not all albums have complete information.
 
-Write a function which takes an id, a property (prop), and a value.
+Write a function which takes an album's id (like 2548), a property prop (like "artist" or "tracks"), and a value (like "Addicted to Love") to modify the data in this collection.
 
-For the given id in collection:
+If prop isn't "tracks" and value isn't empty (""), update or set the value for that record album's property.
 
-If value is non-blank (value !== "") and prop is not "tracks" then update or set the value for the prop.
+Your function must always return the entire collection object.
 
-If the prop is "tracks" and value is non-blank, push the value onto the end of the tracks array.
+There are several rules for handling incomplete data:
 
-If value is blank, delete that prop.
+If prop is "tracks" but the album doesn't have a "tracks" property, create an empty array before adding the new value to the album's corresponding property.
 
-Always return the entire collection object.
+If prop is "tracks" and value isn't empty (""), push the value onto the end of the album's existing tracks array.
 
-Note
-Don't forget to use bracket notation when accessing object properties with variables.
+If value is empty (""), delete the given prop property from the album.
+
+Hints
+Use bracket notation when accessing object properties with variables.
+
+Push is an array method you can read about on Mozilla Developer Network.
+
+You may refer back to Manipulating Complex Objects Introducing JavaScript Object Notation (JSON) for a refresher.
 
 Code by Rafael Rodriguez
 rafase282@gmail.com
@@ -24,46 +30,51 @@ http://www.freecodecamp.com/rafase282
 
 // Setup
 var collection = {
-  2548: {
-    album: "Slippery When Wet",
-    artist: "Bon Jovi",
-    tracks: [
+  "2548": {
+    "album": "Slippery When Wet",
+    "artist": "Bon Jovi",
+    "tracks": [
       "Let It Rock",
       "You Give Love a Bad Name"
     ]
   },
-  2468: {
-    album: "1999",
-    artist: "Prince",
-    tracks: [
+  "2468": {
+    "album": "1999",
+    "artist": "Prince",
+    "tracks": [
       "1999",
       "Little Red Corvette"
     ]
   },
-  1245: {
-    artist: "Robert Palmer",
-    tracks: []
+  "1245": {
+    "artist": "Robert Palmer",
+    "tracks": []
   },
-  5439: {
-    album: "ABBA Gold"
+  "5439": {
+    "album": "ABBA Gold"
   }
 };
 // Keep a copy of the collection for tests
 var collectionCopy = JSON.parse(JSON.stringify(collection));
 
 // Only change code below this line
-function update(id, prop, value) {
+function updateRecords(id, prop, value) {
+  // If the value is empty we just delete the prop and return the collection.
   if (value === '') {
     delete collection[id][prop];
-  } else if (prop !== 'tracks') {
-    collection[id][prop] = value;
-  } else {
-    collection[id][prop].push(value);
+    return collection;
   }
-
-
+  // If we do have a value, handle business!
+  if (prop === 'tracks') {
+    if (collection[id][prop] === undefined) {
+      collection[id][prop] = [];
+    }
+    collection[id][prop].push(value);
+  } else {
+    collection[id][prop] = value;
+  }
   return collection;
 }
 
 // Alter values below to test your code
-update(5439, "artist", "ABBA");
+updateRecords(5439, "artist", "ABBA");
