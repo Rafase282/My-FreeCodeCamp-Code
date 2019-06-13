6f8df52acd5c988f4559a3a1c801b9f7982b3d6f
Chety <cetincelik20@gmail.com>
2019-06-13 14:13:23 +0300

    Update 07 Bonfire DNA Pairing.js
    We can avoid mutating array(via push) and also calling internal function call. This is more functional way.

--- Intermediate Algorithm Scripting/07 Bonfire DNA Pairing.js
+++ Intermediate Algorithm Scripting/07 Bonfire DNA Pairing.js
@@ -19,35 +19,10 @@ Code by Rafael Rodriguez
 rafase282@gmail.com
 http://www.freecodecamp.com/rafase282
 */
+const dnaStore = {A: "T", T: "A", G: "C", C: "G"}
 
 function pair(str) {
-  // Return each strand as an array of two elements, the original and the pair.
-  var paired = [];
-
-  // Function to check with strand to pair.
-  var search = function(char) {
-    switch (char) {
-      case 'A':
-        paired.push(['A', 'T']);
-        break;
-      case 'T':
-        paired.push(['T', 'A']);
-        break;
-      case 'C':
-        paired.push(['C', 'G']);
-        break;
-      case 'G':
-        paired.push(['G', 'C']);
-        break;
-    }
-  };
-
-  // Loops through the input and pair.
-  for (var i = 0; i < str.length; i++) {
-    search(str[i]);
-  }
-
-  return paired;
+  return [...str.split("").map(k => [k,dnaStore[k]])]
 }
 
 pair('GCG');
