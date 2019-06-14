a1176a554ada9714df81a278f1182badbf8f840a
Chety <cetincelik20@gmail.com>
2019-06-14 09:39:27 +0300

    Update 15 Bonfire Smallest Common Multiple.js
    More readable and understanble way.

--- Intermediate Algorithm Scripting/15 Bonfire Smallest Common Multiple.js
+++ Intermediate Algorithm Scripting/15 Bonfire Smallest Common Multiple.js
@@ -17,36 +17,17 @@ http://www.freecodecamp.com/rafase282
 */
 
 function smallestCommons(arr) {
-  // Sort array from greater to lowest
-  // This line of code was from Adam Doyle (http://github.com/Adoyle2014)
-  arr.sort(function(a, b) {
-    return b - a;
-  });
-
-  // Create new array and add all values from greater to smaller from the original array.
-  var newArr = [];
-  for (var i = arr[0]; i >= arr[1]; i--) {
-    newArr.push(i);
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
+  const numbers = [];
+  for(let i = min; i<= max; i++){
+	  numbers.push(i);
   }
-
-  // Variables needed declared outside the loops.
-  var quot = 0;
-  var loop = 1;
-  var n;
-
-  // run code while n is not the same as the array length.
-  do {
-    quot = newArr[0] * loop * newArr[1];
-    for (n = 2; n < newArr.length; n++) {
-      if (quot % newArr[n] !== 0) {
-        break;
-      }
+  for(let i = max;;i += max){
+	if(numbers.every(item => i % item === 0)){
+		return i;
     }
-
-    loop++;
-  } while (n !== newArr.length);
-
-  return quot;
+  }  
 }
 
 smallestCommons([1, 13]);
